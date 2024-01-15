09446e6f3f11b47e7a3656d1167bb1edbdd2e859
Hong <151532167+UChangh@users.noreply.github.com>
2024-01-15 19:54:38 +0900

    add : ContactListFragment.kt / ContactListAdapter.kt

---
+++ app/src/main/java/com/example/contactapp/ContactListAdapter.kt
@@ -0,0 +1,23 @@
+package com.example.contactapp
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.contactapp.databinding.LayoutFragmentContactListBinding
+
+class ContactListAdapter:RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    inner class Holder(binding: LayoutFragmentContactListBinding):RecyclerView.ViewHolder(binding.root) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
