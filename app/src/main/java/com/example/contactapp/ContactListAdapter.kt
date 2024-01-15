1aa66dbbda0e4daee5de9158b4520e299754312d
Hong <151532167+UChangh@users.noreply.github.com>
2024-01-15 20:06:40 +0900

    add : ContactListFragment.kt / ContactListAdapter.kt update : recycler view xml 파일명

--- app/src/main/java/com/example/contactapp/ContactListAdapter.kt
+++ app/src/main/java/com/example/contactapp/ContactListAdapter.kt
@@ -1,23 +1,24 @@
 package com.example.contactapp
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.contactapp.databinding.LayoutFragmentContactListBinding
+import com.example.contactapp.databinding.LayoutRvUserBinding
 
-class ContactListAdapter:RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-    inner class Holder(binding: LayoutFragmentContactListBinding):RecyclerView.ViewHolder(binding.root) {
+class ContactListAdapter:RecyclerView.Adapter<ContactListAdapter.Holder>() {
+    inner class Holder(binding: LayoutRvUserBinding):RecyclerView.ViewHolder(binding.root) {
 
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        TODO("Not yet implemented")
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactListAdapter.Holder {
+        val binding = LayoutRvUserBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return Holder(binding)
     }
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+    override fun onBindViewHolder(holder: ContactListAdapter.Holder, position: Int) {
+
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount(): Int = 0
 }
