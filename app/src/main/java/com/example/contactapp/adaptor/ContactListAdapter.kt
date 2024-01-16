97ec1a3a28a3c55df8ad1d83a511f5dd78c6aede
Hong <151532167+UChangh@users.noreply.github.com>
2024-01-16 10:45:10 +0900

    refactor : layout_rv_user의 별 아이콘 위치, 이름 색 : 기본 -> black, 사진 크기 6dp 증가

--- app/src/main/java/com/example/contactapp/adaptor/ContactListAdapter.kt
+++ app/src/main/java/com/example/contactapp/adaptor/ContactListAdapter.kt
@@ -4,9 +4,10 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.contactapp.R
+import com.example.contactapp.data.ContactData
 import com.example.contactapp.databinding.LayoutRvUserBinding
 
-class ContactListAdapter:RecyclerView.Adapter<ContactListAdapter.Holder>() {
+class ContactListAdapter(private val userDataList:ArrayList<ContactData>):RecyclerView.Adapter<ContactListAdapter.Holder>() {
     inner class Holder(binding: LayoutRvUserBinding):RecyclerView.ViewHolder(binding.root) {
         val image = binding.ivRvUser
         val name = binding.tvRvUserName
@@ -20,10 +21,11 @@ class ContactListAdapter:RecyclerView.Adapter<ContactListAdapter.Holder>() {
     }
 
     override fun onBindViewHolder(holder: Holder, position: Int) {
-        holder.image.setImageResource(R.drawable.user_profile_empty)
-        holder.name.text = "Name"
-        holder.favorite.setImageResource(R.drawable.star_empty)
-        holder.favOnOff = false
+        with(holder) {
+            name.text = userDataList[position].name
+            image.setImageResource(R.drawable.user_profile_empty)
+            favorite.setImageResource(R.drawable.star_empty)
+        }
     }
 
     override fun getItemCount(): Int = 10
--- app/src/main/java/com/example/contactapp/fragment/ContactListFragment.kt
+++ app/src/main/java/com/example/contactapp/fragment/ContactListFragment.kt
@@ -1,11 +1,15 @@
 package com.example.contactapp.fragment
 
+import android.icu.lang.UCharacter.VerticalOrientation
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.contactapp.activity.ContactActivity
 import com.example.contactapp.adaptor.ContactListAdapter
+import com.example.contactapp.data.ContactDatabase
 import com.example.contactapp.databinding.LayoutFragmentContactListBinding
 
 
@@ -19,6 +23,8 @@ class ContactListFragment : Fragment() {
 
     private var _binding:LayoutFragmentContactListBinding? = null
     private val binding get() = _binding!!
+
+    private val mainPage by lazy { context as ContactActivity }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,8 +44,11 @@ class ContactListFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val clAdapter = ContactListAdapter()
-        binding.recyclerView.adapter = clAdapter
+        val clAdapter = ContactListAdapter(ContactDatabase.totalContactData)
+        binding.recyclerView.apply {
+            adapter = clAdapter
+            layoutManager = LinearLayoutManager(mainPage, LinearLayoutManager.VERTICAL, false)
+        }
     }
 
     companion object {
