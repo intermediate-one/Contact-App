332f66b0492adcb68083e37ec78a18fd0ad67dca
kgdrjsen <gundoo3333@gmail.com>
2024-01-16 14:58:19 +0900

    comment : 주석 지우기

--- app/src/main/java/com/example/contactapp/fragment/DetailFragment.kt
+++ app/src/main/java/com/example/contactapp/fragment/DetailFragment.kt
@@ -44,11 +44,7 @@ class DetailFragment : Fragment() {
             arguments?.getParcelable<ContactData>(Contants.IEIM_INDEX)
         }
     }
-
-    // TODO: Rename and change types of parameters
-//    private var param1: String? = null
-//    private var param2: String? = null
-
+//    onCreate 생략가능 -> 데이터를
 //    override fun onCreate(savedInstanceState: Bundle?) {
 //        super.onCreate(savedInstanceState)
 //        arguments?.let {
@@ -62,8 +58,6 @@ class DetailFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-//        return inflater.inflate(R.layout.fragment_detail, container, false)
         _binding = FragmentDetailBinding.inflate(inflater,container,false)
         return binding.root
     }
@@ -88,23 +82,4 @@ class DetailFragment : Fragment() {
         _binding = null
     }
 
-//    companion object {
-//        /**
-//         * Use this factory method to create a new instance of
-//         * this fragment using the provided parameters.
-//         *
-//         * @param param1 Parameter 1.
-//         * @param param2 Parameter 2.
-//         * @return A new instance of fragment DetailFragment.
-//         */
-//        // TODO: Rename and change types and number of parameters
-//        @JvmStatic
-//        fun newInstance(param1: String, param2: String) =
-//            DetailFragment().apply {
-//                arguments = Bundle().apply {
-//                    putString(ARG_PARAM1, param1)
-//                    putString(ARG_PARAM2, param2)
-//                }
-//            }
-//    }
 }
