ac9869ad856e98e12c2054c423c1891fcea51e09
Euny Kim <euny4535@gmail.com>
2024-01-17 12:51:38 +0900

    style : 디테일 액티비티 이미지 수정 및 아이콘 크기 수정 및 레이아웃 수정

---
+++ app/src/main/java/com/example/contactapp/activity/AddContactActivity.kt
@@ -0,0 +1,12 @@
+package com.example.contactapp.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.contactapp.R
+
+class AddContactActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_contact)
+    }
+}
