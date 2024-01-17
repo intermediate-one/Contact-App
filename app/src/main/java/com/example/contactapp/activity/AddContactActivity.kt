ecd4790d88e9212a943898e7f74213d016192bb7
Euny Kim <euny4535@gmail.com>
2024-01-17 20:42:23 +0900

    feat : 그룹 추가 다이얼로그 구현 (그룹 추가 시 입력값 유효성 확인 - 입력값이 비어있거나 입력값이 이미 있는 그룹과 겹치는 경우 토스트 메세지를 띄워줍니다.

--- app/src/main/java/com/example/contactapp/activity/AddContactActivity.kt
+++ app/src/main/java/com/example/contactapp/activity/AddContactActivity.kt
@@ -1,5 +1,7 @@
 package com.example.contactapp.activity
 
+import android.content.Context
+import android.content.DialogInterface
 import android.media.ImageReader
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -8,7 +10,10 @@ import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.core.widget.addTextChangedListener
+import com.example.contactapp.R
 import com.example.contactapp.data.AddContactErrorMessage
 import com.example.contactapp.data.AddContactValidExtension.includeKorean
 import com.example.contactapp.data.AddContactValidExtension.includeNumberWithDash
@@ -16,6 +21,7 @@ import com.example.contactapp.data.AddContactValidExtension.includeValidAddress
 import com.example.contactapp.data.AddContactValidExtension.includeValidEmail
 import com.example.contactapp.data.AddContactValidExtension.includeValidMbti
 import com.example.contactapp.data.AddContactValidExtension.includeValidMemo
+import com.example.contactapp.data.ContactDatabase.addGroup
 import com.example.contactapp.data.ContactDatabase.groupData
 import com.example.contactapp.databinding.ActivityAddContactBinding
 
@@ -50,7 +56,55 @@ class AddContactActivity : AppCompatActivity() {
         setOnFocusChangedListener()
         onClickButtonComplete()
         onClickButtonBack()
+
         setGroupProvider()
+        addGroupBtn()
+    }
+
+    private fun addGroupBtn() {
+        binding.ibtnAddContactAddGroup.setOnClickListener {
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle(getString(R.string.dialog_add_group))
+            builder.setMessage(getString(R.string.dialog_new_group))
+            builder.setIcon(R.mipmap.ic_launcher)
+
+            val v1 = layoutInflater.inflate(R.layout.dialog_add_group, null)
+            builder.setView(v1)
+
+            // p0에 해당 AlertDialog가 들어온다. findViewById를 통해 view를 가져와서 사용
+            val listener = DialogInterface.OnClickListener { p0, p1 ->
+                val alert = p0 as AlertDialog
+                val newGroup: EditText? = alert.findViewById<EditText>(R.id.et_dialog_add_group)
+
+                // 새로운 그룹 이름으로 입력 된 값이 비어 있거나 이미 있는 그룹을 입력하고
+                // 확인을 누르면 토스트 메세지를 띄우도록 만들어보았습니다.
+                // TODO : 아직 작동이 잘 되는지 확인이 안되었습니당.
+                if (p1 == DialogInterface.BUTTON_POSITIVE) {
+                    groupData.forEach {
+                        if (newGroup?.text.toString().isEmpty()) {
+                            shortToastMessage(this,R.string.dialog_empty_text_message)
+                        } else if (newGroup?.text.toString() == it) {
+                            shortToastMessage(this,R.string.dialog_existing_group_message)
+                        } else {
+                            newContactGroup = newGroup?.text.toString()
+                            addGroup(newContactGroup)
+                        }
+                    }
+                }
+
+                newContactGroup = newGroup?.text.toString()
+            }
+
+            builder.setPositiveButton(getString(R.string.dialog_confirm_add_group), listener)
+            builder.setNegativeButton(getString(R.string.dialog_cancel_add_group), null)
+
+            builder.show()
+
+        }
+    }
+
+    private fun shortToastMessage(context: Context, message: Int) {
+        Toast.makeText(context,getString(message),Toast.LENGTH_SHORT).show()
     }
 
     // 스피너에서 현재 있는 그룹 리스트를 표시해주는 함수.
