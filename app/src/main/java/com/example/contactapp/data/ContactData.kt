f3f13458a5c24a1b3a0b03099553ffef92ddd3e5
Euny Kim <euny4535@gmail.com>
2024-01-17 17:13:29 +0900

    feat : 데이터베이스 데이터클래스 SA변경사항 반영 + DUmmyData 또한 다양하게 반영

--- app/src/main/java/com/example/contactapp/data/ContactData.kt
+++ app/src/main/java/com/example/contactapp/data/ContactData.kt
@@ -8,11 +8,11 @@ data class ContactData (
     var name: String,
     var profileImage: Int,
     var phoneNumber: String,
-    var email: String,
-    var group: Int,
-    var birthday: Int,
-    var mbti: String,
-    var memo: String,
+    var email: String?,
+    var group: String?,
+    var birthday: Int?,
+    var mbti: String?,
+    var memo: String?,
     var notification: Int?,
     var favorite: Boolean
 ):Parcelable
