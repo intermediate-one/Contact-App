bf1689ae3f1b20af19954495695f2b6731943ce1
Hong <151532167+UChangh@users.noreply.github.com>
2024-01-18 17:03:53 +0900

    fix : 연락처 좋아요 클릭이 되지 않던 문제 해결

--- app/src/main/java/com/example/contactapp/adaptor/ContactListAdapter.kt
+++ app/src/main/java/com/example/contactapp/adaptor/ContactListAdapter.kt
@@ -3,20 +3,13 @@ package com.example.contactapp.adaptor
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.contactapp.R
 import com.example.contactapp.data.ContactData
 import com.example.contactapp.databinding.LayoutRvUserBinding
-import androidx.recyclerview.widget.DiffUtil
-import androidx.viewbinding.ViewBinding
-import com.example.contactapp.data.ContactDatabase
-import com.example.contactapp.databinding.ActivityContactBinding
-import com.example.contactapp.databinding.FragmentContactListBinding
 import com.example.contactapp.databinding.LayoutRvUserGridBinding
 import com.example.contactapp.fragment.ContactListFragment.Companion.listGrid
 import com.example.contactapp.fragment.ContactListFragment.Companion.userPosition
-import java.lang.Exception
 
 
 private const val LINEAR_LAYOUT = 1
@@ -77,23 +70,22 @@ class ContactListAdapter(private val userDataList:ArrayList<ContactData>):Recycl
                 with(holdList) {
                     name.text = userDataList[position].name
                     image.setImageResource(userDataList[position].profileImage)
-                    when (userDataList[position].favorite) {
-                        true -> {
-                            favorite.setImageResource(R.drawable.star_full)
-                            favorite.setOnClickListener {
+                    when(userDataList[position].favorite) {
+                        true -> favorite.setImageResource(R.drawable.star_full)
+                        false -> favorite.setImageResource(R.drawable.star_empty)
+                    }
+                    favorite.setOnClickListener {
+                        when (userDataList[position].favorite) {
+                            true -> {
                                 userDataList[position].favorite = false
                                 favorite.setImageResource(R.drawable.star_empty)
                             }
-                        }
-                        false -> {
-                            favorite.setImageResource(R.drawable.star_empty)
-                            favorite.setOnClickListener {
+                            false -> {
                                 userDataList[position].favorite = true
                                 favorite.setImageResource(R.drawable.star_full)
                             }
                         }
                     }
-                    userPosition = position     // 현재 선택된 아이템의 Position을 userPosition이라는 companion object에 저장
                 }
             }
             -1 -> {
@@ -101,23 +93,22 @@ class ContactListAdapter(private val userDataList:ArrayList<ContactData>):Recycl
                 with (holdGrid) {
                     name.text = userDataList[position].name
                     image.setImageResource(userDataList[position].profileImage)
-                    when (userDataList[position].favorite) {
-                        true -> {
-                            favorite.setImageResource(R.drawable.star_full)
-                            favorite.setOnClickListener {
+                    when(userDataList[position].favorite) {
+                        true -> favorite.setImageResource(R.drawable.star_full)
+                        false -> favorite.setImageResource(R.drawable.star_empty)
+                    }
+                    favorite.setOnClickListener {
+                        when (userDataList[position].favorite) {
+                            true -> {
                                 userDataList[position].favorite = false
                                 favorite.setImageResource(R.drawable.star_empty)
                             }
-                        }
-                        false -> {
-                            favorite.setImageResource(R.drawable.star_empty)
-                            favorite.setOnClickListener {
+                            false -> {
                                 userDataList[position].favorite = true
                                 favorite.setImageResource(R.drawable.star_full)
                             }
                         }
                     }
-                    userPosition = position
                 }
             }
             else -> throw Exception("Holder를 Casting 할 수 없습니다.")
