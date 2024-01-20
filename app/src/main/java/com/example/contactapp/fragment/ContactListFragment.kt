1d3d6e6ffff5484b0252afd701834d06e578b8a3
jericho31 <a@a.a>
2024-01-20 14:32:41 +0900

    fix : 상세페이지 별 누르고 돌아왔을 때 리스트 버그 생기는거 수정 (+ 널값 터지는거)

--- app/src/main/java/com/example/contactapp/fragment/ContactListFragment.kt
+++ app/src/main/java/com/example/contactapp/fragment/ContactListFragment.kt
@@ -3,7 +3,6 @@ package com.example.contactapp.fragment
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,7 +11,6 @@ import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.contactapp.R
@@ -74,13 +72,15 @@ class ContactListFragment : Fragment() {
                             listGrid = -1
                             layoutManager = LinearLayoutManager(mainPage, LinearLayoutManager.VERTICAL, false)
                             btnListGrid.setImageResource(R.drawable.icon_grid_black)    // 현재가 list니 버튼을 누르면 Grid로 바꿀 수 있다는 것을 미리 보여주기 위해
-                            clAdapter.notifyItemRangeChanged(userPosition,sortedList.size)
+                            notifyDataSetChangedStayedScroll()  //ddd
+//                            clAdapter.notifyItemRangeChanged(userPosition,sortedList.size)
                         }
                         -1 -> {
                             listGrid = 1
                             layoutManager = GridLayoutManager(mainPage, 3, GridLayoutManager.VERTICAL, false)
                             btnListGrid.setImageResource(R.drawable.icon_list_black)
-                            clAdapter.notifyItemRangeChanged(userPosition,sortedList.size)
+                            notifyDataSetChangedStayedScroll()  //ddd
+//                            clAdapter.notifyItemRangeChanged(userPosition,sortedList.size)
                         }
                     }
                 }
@@ -88,8 +88,8 @@ class ContactListFragment : Fragment() {
             //Detail에서 받았을 때
             activityResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
                 if (it.resultCode == AppCompatActivity.RESULT_OK) {
-                    val isFavorite = it.data?.getBooleanExtra("isFavorite",false) as Boolean
-                    val itemNum = it.data?.getIntExtra(Contants.ITEM_INDEX,0) as Int
+                    val isFavorite = it.data?.getBooleanExtra("isFavorite",false) ?: false
+                    val itemNum = it.data?.getIntExtra(Contants.ITEM_INDEX,0) ?: 0
 
                     if (isFavorite) {
                         sortedList[itemNum].favorite = true
@@ -99,11 +99,12 @@ class ContactListFragment : Fragment() {
                         }
                     }
                     //어댑터 갱신해주는 코드
-                    clAdapter.notifyItemChanged(itemNum)
+                    notifyDataSetChangedStayedScroll()  //ddd
+//                    clAdapter.notifyItemChanged(itemNum)
                 //수정된 값 받아오기
                 }else if (it.resultCode == AppCompatActivity.RESULT_FIRST_USER) {
-                    val isFavorite = it.data?.getBooleanExtra("isFavorite",false) as Boolean
-                    val itemNum = it.data?.getIntExtra(Contants.ITEM_INDEX,0) as Int
+                    val isFavorite = it.data?.getBooleanExtra("isFavorite",false) ?: false
+                    val itemNum = it.data?.getIntExtra(Contants.ITEM_INDEX,0) ?: 0
                     val data: ContactData? by lazy {
                         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
                             arguments?.getParcelable(Contants.ITEM_DATA, ContactData::class.java)
@@ -124,12 +125,13 @@ class ContactListFragment : Fragment() {
                     sortedList[itemNum].address = data?.address ?: "이름"
                     sortedList[itemNum].birthday = data?.birthday ?: "이름"
                     sortedList[itemNum].mbti = data?.mbti ?: "이름"
-                    sortedList[itemNum].notification = data?.notification ?: null
+                    sortedList[itemNum].notification = data?.notification
                     sortedList[itemNum].phoneNumber = data?.phoneNumber ?: "010-1234-1234"
                     sortedList[itemNum].memo = data?.memo ?: "010-1234-1234"
-                    sortedList[itemNum].profileImage = data?.profileImage as Int
+                    sortedList[itemNum].profileImage = data?.profileImage ?: -1
                     userPosition = itemNum
-                    clAdapter.notifyItemRangeChanged(userPosition,sortedList.size)
+                    notifyDataSetChangedStayedScroll()  //ddd
+//                    clAdapter.notifyItemRangeChanged(userPosition,sortedList.size)
                 }
             }
             //Detail로 보내고 다시 값 받기
@@ -186,5 +188,17 @@ class ContactListFragment : Fragment() {
             val intent = Intent(activity, AddContactActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    /** 초필살기 (디버그용) */
+    private fun notifyDataSetChangedStayedScroll() {
+        binding.recyclerView.adapter?.notifyDataSetChanged()
+
+//        binding.recyclerView.also {
+//            // 스크롤 포지션 유지하려고 state 했는데, 스크롤 유지가 안되네
+//            val beforeState = it.layoutManager?.onSaveInstanceState()
+//            it.adapter?.notifyDataSetChanged()  //ddd
+//            it.layoutManager?.onRestoreInstanceState(beforeState)
+//        }
     }
 }
